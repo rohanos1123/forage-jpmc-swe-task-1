b5084d91650635817f5c0471205ee8d34362feb4
Rohan Rao <76537742+rohanos1123@users.noreply.github.com>
2023-10-07 12:43:46 -0400

    Completed Part 1

--- client_test.py
+++ client_test.py
@@ -1,5 +1,5 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint, getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,6 +8,9 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes: 
+        self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price'])/2))
+
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -16,9 +19,32 @@ class ClientTest(unittest.TestCase):
     ]
     """ ------------ Add the assertion below ------------ """
 
+    for quote in quotes: 
+      self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price']+ quote['top_ask']['price'])/2))
+
 
   """ ------------ Add more unit tests ------------ """
 
+  def test_getRatioWithZeroesDenom(self):
+    test_prices = [{
+      "A" : 10, 
+      "B" : 0, 
+    }, 
+    {
+      "A" : 0, 
+      "B" : 0
+    }
+    ]
+    for test in test_prices: 
+      self.assertRaises(ZeroDivisionError, getRatio, test["A"], test["B"])
+
+    
+  def test_getRatioWith0Num(self): 
+    test_price =  {
+      "A" : 0,
+      "B" : 10
+    }
+    self.assertEqual(getRatio(test_price["A"], test_price["B"]), 0)
 
 
 if __name__ == '__main__':
